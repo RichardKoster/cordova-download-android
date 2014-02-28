a8ede2fa3f1f03fb7b9fbad084d9de0800d2dcbf
Richard Köster <rhm.koster@gmail.com>
2014-02-28 16:23:11 +0100

    fix

--- www/download.js
+++ www/download.js
@@ -1,5 +1,8 @@
-var downloadplugin = {
-    download: function(fileUrl, successCallback, errorCallback){
-        cordova.exec(successCallback, errorCallback, 'DownloadPlugin',"download",[fileUrl]);
-    }
+var DownloadPlugin = function(){
+
 }
+DownloadPlugin.prototype.download = function (fileUrl, successCallback, errorCallback) {
+	cordova.exec(successCallback, errorCallback, 'DownloadPlugin', "download", [fileUrl]);
+}
+
+module.exports = DownloadPlugin;
